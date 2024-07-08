54ba79f7f560d9be82963b3a82ec70d6df4d5a95
Kuroashi1995 <kuroashi.prog@gmail.com>
2024-07-08 16:03:51 -0400

    housekeeping

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,14 +6,7 @@ import { div } from "three/examples/jsm/nodes/Nodes.js";
 
 function Footer() {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
-          src="/footer-grid.svg"
-          alt="grid"
-          className="w-full h-full opacity-50"
-        />
-      </div>
+    <footer className="w-full mb-[100px] pb-10 md:mb-5" id="contact">
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
           Let&apos;s put <span className="text-purple">your</span> ideas into
