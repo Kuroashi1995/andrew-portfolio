6821af683da13819eeb1b1453d411e245d9b1f43
Kuroashi1995 <kuroashi.prog@gmail.com>
2024-07-08 15:31:09 -0400

    fix some components

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,29 @@
 import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { navItems } from "@/data";
-import { FaHome } from "react-icons/fa";
+import { HiHome } from "react-icons/hi";
+import { MdMail } from "react-icons/md";
+import { VscRepo } from "react-icons/vsc";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems} />
+        <FloatingNav
+          navItems={navItems}
+          icons={[<HiHome />, <VscRepo />, <MdMail />]}
+        />
         <Hero />
         <Grid />
         <RecentProjects />
         <Experience />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
